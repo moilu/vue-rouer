f3a0a4f233685fd1e0364a999adedf95f819b772
moilu <moisesluna0390@gmail.com>
2022-02-10 23:01:58 -0800

    adds lazy loading components to router

---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <router-link to="/">Pokemon List</router-link>
+        <router-link to="/id">Pokemon por id</router-link>
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+<style scoped>
+div {
+    padding: 0 50px;
+}
+
+div a {
+    font-weight: bold;
+    color: #2c3e50;
+    margin: 0 10px;
+}
+
+a.router-link-exact-active {
+    color: #42b983;
+}
+
+</style>
